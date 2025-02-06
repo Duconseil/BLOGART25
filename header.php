9d0c68eb5d9deab40a44ee05e6df6a82bf81a1d6
Duconseil <chaduconseil@gmail.com>
2025-02-06 11:00:22 +0100

    MAJ

--- header.php
+++ header.php
@@ -23,8 +23,8 @@ $numStat = $_SESSION['numStat'] ?? null;  // Récupérer numStat ou null si non 
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
-          crossorigin="anonymous" />
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
+            crossorigin="anonymous" />
 
     <link rel="shortcut icon" type="image/x-icon" href="src/images/article.png" />
 </head>
@@ -85,7 +85,7 @@ $numStat = $_SESSION['numStat'] ?? null;  // Récupérer numStat ou null si non 
 
             <?php else: ?>
                 <a class="btn btn-primary m-1" href="/views/backend/security/login.php" role="button">Se connecter</a>
-                <a class="btn btn-dark m-1" href="/views/backend/security/signup.php" role="button">S'incrire</a>
+                <a class="btn btn-dark m-1" href="/views/backend/security/signup.php" role="button">S'inscrire</a>
             <?php endif; ?>
         </div>
     </div>
