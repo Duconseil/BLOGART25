6e6aa0afc8e9bd75121cbc0bd6b3c0ca22c8ac5a
Duconseil <chaduconseil@gmail.com>
2025-02-06 10:42:24 +0100

    MAJ

--- views/backend/security/login.php
+++ views/backend/security/login.php
@@ -4,18 +4,6 @@ include '../../../header.php';
 
 global $DB;
 
-// Vérifiez si la connexion à la base de données est déjà initialisée
-if ($DB === null) {
-    try {
-        $DB = new PDO('mysql:host=localhost;dbname=BLOGART25', 'root', 'root', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false
-        ]);
-    } catch (PDOException $e) {
-        die("Impossible de se connecter à la base de données: " . $e->getMessage());
-    }
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Vérification des informations de connexion
